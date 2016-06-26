63b44825913c7c342b5f39c5df90a5a72a762fe1
Shreyas Skandan <shreyas.ibanez@gmail.com>
2016-06-25 21:07:12 -0400

    no message

--- parse_data.m
+++ parse_data.m
@@ -7,11 +7,13 @@ mainDir = '/Users/shreyasskandan/Desktop/ReflectanceData/Fruits/';
 fruitExt = {'1_GD/','2_GS/','3_HC/','4_OR','5_PL','6_RD','7_ST'};
 numClasses = length(fruitExt);
 DirList = cell(numClasses,1);
+
 % Store filenames into directory list
 for dirctr = 1:numClasses 
     DirList{dirctr} = dir(strcat(mainDir,char(fruitExt(dirctr)),'/*.txt'));
 end
 
+% Load data into variables and save to mat file
 for fruits = 1:length(DirList)
     fruitfilename = sprintf('fruit%d.mat',fruits);
     for instance = 1:length(DirList{fruits})
